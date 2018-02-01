4966a4cf8e940579987ebb5f0e6f91500b8b5924
mfurqon27 <akunkhusus42@gamil.com>
2018-02-01 17:13:24 +0700

    update navbar

--- app/src/main/java/camp/bso/inf/scorringboard/SplashActivity.java
+++ app/src/main/java/camp/bso/inf/scorringboard/SplashActivity.java
@@ -21,7 +21,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                Intent i = new Intent(SplashActivity.this, NavigationBar.class);
                 startActivity(i);
 
                 finish();
